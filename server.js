a26e71ac38d16121466ff6df0ff930cd126e9204
chlucas324 <chlucas324@gmail.com>
2022-05-14 19:26:31 -0400

    update for new notes and port listening

--- server.js
+++ server.js
@@ -14,8 +14,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //route to notes.html
-app.get("/notes", function (req, res) {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
+// return saved notes as JSON
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+});
 
+// route to index.html
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// receive and return new note
+app.post('/api/notes', (req, res) => {
+    let newNote = {
+        id: id,
+        title: req.body.title,
+        text: req.body.text
+    };
+    let noteList = JSON.parse(fs.readFileSync('./db/db/json', 'utf8'));
+    let noteLength = (noteList.length).toString();
+});
+
+// push updated note
+noteList.push(newNote);
+
+// write updated data to db/db.json
+fs.writeFileSync('./db/db.json', JSON.stringify(noteList));
+res.json(noteList);
+
+//start server to begin listening
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
