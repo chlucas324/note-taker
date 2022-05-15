984d68bf034720752607e988683f8937b452c62d
chlucas324 <chlucas324@gmail.com>
2022-05-15 18:06:06 -0400

    untracked node_modules

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 //dependencies
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
-//set up Express App to listen on port 3001
-const PORT = process.env.PORT || 3001;
+//set up Express App to listen on port 8000
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 
 // Set up Express App to handle data parsing
-app.use(express.static('/public'));
-// app.use(express.static('./'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('./'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
