25c8ca1fa7a31215d83797ee57ac476f9dfe1533
chlucas324 <chlucas324@gmail.com>
2022-05-15 17:49:16 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 //dependencies
-const fs = require('fs');
 const express = require('express');
-
+// const fs = require('fs');
 
 //set up Express App to listen on port 3001
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 
 // Set up Express App to handle data parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static('./'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 //require routes.js files
-require('./Routes/apiRoutes')(app);
-require('./Routes/htmlRoutes')(app);
+require('./Routes/apiRoutes');
+require('./Routes/htmlRoutes');
 
 //start server to begin listening
 app.listen(PORT, () => {
