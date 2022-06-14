c263bdfd73b900a5cc5e465db22256ec29135c78
chlucas324 <chlucas324@gmail.com>
2022-06-14 18:13:06 -0400

    clean up code

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -3,13 +3,12 @@ const path = require("path");
 
 //route to notes.html
 module.exports = (app) => {
-app.get("/notes", (req, res) => {
+  app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
+  });
 
-// route to index.html
-app.get('*', (req,res) => {
+  // route to index.html
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+  });
 };
-
--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 //dependencies
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
+
 
 //set up Express App to listen on port 3001
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
 // Set up Express App to handle data parsing
-app.use(express.static(__dirname + '/public'));
-app.use(express.static('./'));
+app.use(express.static(__dirname + "/public"));
+app.use(express.static("./"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 //require routes.js files
-require('./apiRoutes');
-require('./htmlRoutes');
+require("./apiRoutes")(app);
+require("./htmlRoutes")(app);
 
 //start server to begin listening
 app.listen(PORT, () => {
-    console.log(`Server now on port ${PORT}!`)
+  console.log(`Server now on port ${PORT}!`);
 });
